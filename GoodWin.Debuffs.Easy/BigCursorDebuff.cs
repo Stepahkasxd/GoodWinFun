052abec911d0d02a1e9cd8a9bc744797ef23e680
Stepahkasxd <88252722+Stepahkasxd@users.noreply.github.com>
2025-08-09 16:19:13 +0300

    Fix debuff behaviors and input handling

--- GoodWin.Debuffs.Easy/BigCursorDebuff.cs
+++ GoodWin.Debuffs.Easy/BigCursorDebuff.cs
@@ -10,23 +10,29 @@ namespace GoodWin.Debuffs.Easy
         private const int Duration = 60;
         public override string Name => "Большой курсор";
 
-        private readonly int _applyButton;
-        private readonly int _removeButton;
+        private readonly int _autoOffButton;
+        private readonly int _scaleBigButton;
+        private readonly int _scaleResetButton;
+        private readonly int _autoOnButton;
 
         public BigCursorDebuff()
         {
-            _applyButton = JoyCommandService.Instance.Register("cl_auto_cursor_scale 0; cl_cursor_scale 30");
-            _removeButton = JoyCommandService.Instance.Register("cl_cursor_scale 1; cl_auto_cursor_scale 1");
+            _autoOffButton = JoyCommandService.Instance.Register("cl_auto_cursor_scale 0");
+            _scaleBigButton = JoyCommandService.Instance.Register("cl_cursor_scale 30");
+            _scaleResetButton = JoyCommandService.Instance.Register("cl_cursor_scale 1");
+            _autoOnButton = JoyCommandService.Instance.Register("cl_auto_cursor_scale 1");
         }
 
         public override void Apply()
         {
-            JoyCommandService.Instance.Press(_applyButton);
+            JoyCommandService.Instance.Press(_autoOffButton);
+            JoyCommandService.Instance.Press(_scaleBigButton);
             Console.WriteLine($"[BigCursor] applied for {Duration}s");
         }
         public override void Remove()
         {
-            JoyCommandService.Instance.Press(_removeButton);
+            JoyCommandService.Instance.Press(_scaleResetButton);
+            JoyCommandService.Instance.Press(_autoOnButton);
             Console.WriteLine("[BigCursor] restored");
         }
     }
