74138f3cb2f913f902581ef40a4fd164303e0a02
Stepahkasxd <88252722+Stepahkasxd@users.noreply.github.com>
2025-08-09 14:51:36 +0300

    feat: add JoyCommandService self-test and fallback

--- GoodWin.Gui/Services/DotaCommandService.cs
+++ GoodWin.Gui/Services/DotaCommandService.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using GoodWin.Utils;
+using GoodWin.Gui.Views;
 
 namespace GoodWin.Gui.Services
 {
     public class DotaCommandService
     {
+        public bool ConsoleDebuffsEnabled { get; private set; } = true;
+
         public async Task InitializeCommandsAsync(CancellationToken token)
         {
             var start = DateTime.UtcNow;
@@ -19,6 +23,18 @@ namespace GoodWin.Gui.Services
             }
 
             await JoyCommandService.Instance.InitializeBindingsAsync(token);
+
+            if (!JoyCommandService.Instance.IsOperational || !JoyCommandService.Instance.SelfTest())
+            {
+                ConsoleDebuffsEnabled = false;
+                DebugLogService.Log("JoyCommandService unavailable. Console debuffs disabled.");
+                Application.Current?.Dispatcher.Invoke(() =>
+                {
+                    var win = new DebuffNotificationWindow("ViGEm недоступен", "Консольные дебаффы отключены");
+                    win.Show();
+                    Task.Delay(3000).ContinueWith(_ => win.Dispatcher.Invoke(win.Close));
+                });
+            }
         }
     }
 }
